d40628521f910abb0a75d4860f5c5e495d874df8
sabotzs <gkuklev98@gmail.com>
2020-03-29 19:57:54 +0300

    Add get_announcements

--- carre.py
+++ carre.py
@@ -1,14 +1,14 @@
 class Carre:
-	def __init__(self, cards):
-		self.cards = cards
+    def __init__(self, cards):
+        self.cards = cards
 
-	def __int__(self):
-		if self.cards[0].face == 'J':
-			return 200
-		if self.cards[0].face == '9':
-			return 150
-		else:
-			return 100
+    def __int__(self):
+        if self.cards[0].face == 'J':
+            return 200
+        if self.cards[0].face == '9':
+            return 150
+        else:
+            return 100
 
-	def __repr__(self):
-		return 'carre'
+    def __repr__(self):
+        return str(self.cards)
--- test_announcement.py
+++ test_announcement.py
@@ -2,47 +2,165 @@ import unittest
 from announcement import Announcement
 from card import Card
 from consecutive import Consecutive
+from carre import Carre
 
 class TestAnnouncement(unittest.TestCase):
-	def test_get_consecutive_passes_with_two_tierces_from_same_suit(self):
-		ann = Announcement()
-		hand = [
-			[
-				Card('7','c'), Card('8','c'), Card('9','c'),
-				Card('Q','c'), Card('K','c'), Card('A','c')
-			]
-		]
+    def test_get_consecutive_passes_with_two_tierces_from_same_suit(self):
+        ann = Announcement()
+        hand = [
+            [
+                Card('7','c'), Card('8','c'), Card('9','c'),
+                Card('Q','c'), Card('K','c'), Card('A','c')
+            ]
+        ]
 
-		result = ann.get_consecutives(hand)
+        result = ann.get_consecutives(hand)
 
-		self.assertEqual(result, [
-			Consecutive([Card('7','c'), Card('8','c'), Card('9','c')]),
-			Consecutive([Card('Q','c'), Card('K','c'), Card('A','c')])
-		])
+        self.assertEqual(result, [
+            Consecutive([Card('7','c'), Card('8','c'), Card('9','c')]),
+            Consecutive([Card('Q','c'), Card('K','c'), Card('A','c')])
+        ])
 
-	def test_get_consecutive_passes_with_tierce_and_quinte(self):
-		ann = Announcement()
-		hand = [
-			[Card('7','c'), Card('8','c'), Card('9','c')],
-			[Card('7','s'), Card('8','s'), Card('9','s'), Card('10', 's'), Card('J','s')]
-		]
-		
-		result = ann.get_consecutives(hand)
+    def test_get_consecutive_passes_with_tierce_and_quinte(self):
+        ann = Announcement()
+        hand = [
+            [Card('7','c'), Card('8','c'), Card('9','c')],
+            [Card('7','s'), Card('8','s'), Card('9','s'), Card('10', 's'), Card('J','s')]
+        ]
+        
+        result = ann.get_consecutives(hand)
 
-		self.assertEqual(result, [
-			Consecutive([Card('7','c'), Card('8','c'), Card('9','c')]),
-			Consecutive([Card('7','s'), Card('8','s'), Card('9','s'), Card('10', 's'), Card('J','s')])
-		])
+        self.assertEqual(result, [
+            Consecutive([Card('7','c'), Card('8','c'), Card('9','c')]),
+            Consecutive([Card('7','s'), Card('8','s'), Card('9','s'), Card('10', 's'), Card('J','s')])
+        ])
 
-	def test_are_consecutive(self):
-		ann = Announcement()
-		card = Card('9', 's')
-		card1 = Card('Q', 's')
-		card2 = Card('K', 's')
+    def test_check_consecutives_and_carres_passes_with_carre_of_100_points_and_quinte(self):
+        ann = Announcement()
+        carres = [
+            Carre(
+                [
+                    Card('10', 'c'),
+                    Card('10', 'd'),
+                    Card('10', 'h'), 
+                    Card('10', 's')
+                ]
+            )
+        ]
+        consecs = [
+            Consecutive(
+                [
+                    Card('10', 's'),
+                    Card('J', 's'),
+                    Card('Q', 's'),
+                    Card('K', 's'),
+                    Card('A', 's')
+                ]
+            )
+        ]
 
-		self.assertTrue(ann.are_consecutive(card1, card2))
-		self.assertFalse(ann.are_consecutive(card, card1))
+        expected_consecs = consecs
+        ann.check_consecutive_and_carres(consecs, carres)
+
+        self.assertEqual(carres, [])
+        self.assertEqual(consecs, expected_consecs)
+
+    def test_check_consecutives_and_carres_passes_with_carre_of_jacks_and_quinte(self):
+        ann = Announcement()
+        carres = [
+            Carre(
+                [
+                    Card('J', 'c'),
+                    Card('J', 'd'),
+                    Card('J', 'h'), 
+                    Card('J', 's')
+                ]
+            )
+        ]
+        consecs = [
+            Consecutive(
+                [
+                    Card('10', 's'),
+                    Card('J', 's'),
+                    Card('Q', 's'),
+                    Card('K', 's'),
+                    Card('A', 's')
+                ]
+            )
+        ]
+
+        expected_carres = carres
+        ann.check_consecutive_and_carres(consecs, carres)
+
+        self.assertEqual(carres, expected_carres)
+        self.assertEqual(consecs, [])
+
+    def test_check_consecutives_and_carres_passes_with_carre_and_tierce_with_non_empty_intersection(self):
+        ann = Announcement()
+        carres = [
+            Carre(
+                [
+                    Card('J', 'c'),
+                    Card('J', 'd'),
+                    Card('J', 'h'), 
+                    Card('J', 's')
+                ]
+            )
+        ]
+        consecs = [
+            Consecutive(
+                [
+                    Card('10', 's'),
+                    Card('J', 's'),
+                    Card('Q', 's')
+                ]
+            )
+        ]
+
+        expected_carres = carres
+        ann.check_consecutive_and_carres(consecs, carres)
+
+        self.assertEqual(carres, expected_carres)
+        self.assertEqual(consecs, [])
+
+    def test_check_consecutives_and_carres_passes_with_carre_and_tierce_with_empty_intersection(self):
+        ann = Announcement()
+        carres = [
+            Carre(
+                [
+                    Card('J', 'c'),
+                    Card('J', 'd'),
+                    Card('J', 'h'), 
+                    Card('J', 's')
+                ]
+            )
+        ]
+        consecs = [
+            Consecutive(
+                [
+                    Card('7', 's'),
+                    Card('8', 's'),
+                    Card('9', 's')
+                ]
+            )
+        ]
+
+        expected_carres = carres
+        expected_consecs = consecs
+        ann.check_consecutive_and_carres(consecs, carres)
+
+        self.assertEqual(carres, expected_carres)
+        self.assertEqual(consecs, consecs)
+
+    def test_are_consecutive(self):
+        ann = Announcement()
+        card = Card('9', 's')
+        card1 = Card('Q', 's')
+        card2 = Card('K', 's')
+
+        self.assertTrue(ann.are_consecutive(card1, card2))
+        self.assertFalse(ann.are_consecutive(card, card1))
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
