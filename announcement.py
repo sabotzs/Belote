2e66971bb13317e89ee99d6b56d86b3340195374
Zlatina Cholakova <zlatinatc99@gmail.com>
2020-03-29 19:42:04 +0300

    changes

--- announcement.py
+++ announcement.py
@@ -1,66 +1,69 @@
 from consecutive import Consecutive
 from card import list_of_faces
+from carre import Carre
 
 class Announcement:
-	def get_consecutive(self, hand):
-		cons = []
-		
-		for suit in hand:
-			counter = 1
-			for i in range(len(suit)-1):
-				if self.are_consecutive(suit[i], suit[i+1]):
-					counter += 1
-				elif counter > 2:
-					element = Consecutive(suit[i - counter + 1:i + 1])
-					cons.append(element)
-					counter = 1
-			if counter > 2:
-				element = Consecutive(suit[len(suit)-counter:])
-				cons.append(element)
+    def get_consecutive(self, hand):
+        cons = []
+        
+        for suit in hand:
+            counter = 1
+            for i in range(len(suit)-1):
+                if self.are_consecutive(suit[i], suit[i+1]):
+                    counter += 1
+                elif counter > 2:
+                    element = Consecutive(suit[i - counter + 1:i + 1])
+                    cons.append(element)
+                    counter = 1
+            if counter > 2:
+                element = Consecutive(suit[len(suit)-counter:])
+                cons.append(element)
 
-		return cons
+        return cons
 
-	def get_belotes(self, hand):
-		belotes = []
-		
-		for suit in hand:
-			for i in range(len(suit)+1):
-				if suit[i].face == 'Q' and suit[i+1] == 'K':
-					belote.append(Belote([suit[i],suit[i+1]]))
-		
-		return belotes
+    def get_belotes(self, hand):
+        belotes = []
+        
+        for suit in hand:
+            for i in range(len(suit)+1):
+                if suit[i].face == 'Q' and suit[i+1] == 'K':
+                    belote.append(Belote([suit[i],suit[i+1]]))
+        
+        return belotes
 
-	def get_carres(self, hand):
-		carres = []
+    def get_carres(self, hand):
+        carres = []
 
-		for club in hand[0]:
-			helper = [club]
-			for i in range(1,3):
-				for card in hand[i]:
-					if card.face == club.face:
-						helper.append(card)
-			if len(helper) == 4:
-				carres.append(Carre([helper]))
+        for club in hand[0]:
+            if club.face != '7' and club.face != '8':
+                helper = [club]
+                for i in range(1,4):
+                    for card in hand[i]:
+                        if card.face == club.face:
+                            helper.append(card)
+                if len(helper) == 4:
+                    element = Carre(helper)
+                    carres.append(element)
 
-		return carres
+        return carres
 
-	def check_consecutive_and_carres(self,consecs, carres):
-		pass
+    def check_consecutive_and_carres(self,consecs, carres):
+        pass
 
 
-	def get_announcements(self,hand):
-		consecutive = self.get_consecutives(hand)
-		carres = self.get_carres(hand)
-		#TO DO
-		belotes = self.get_belotes(hand)
-		announcements = []
-		#TO DO
+    def get_announcements(self,hand):
+        consecutive = self.get_consecutives(hand)
+        carres = self.get_carres(hand)
+        #TO DO
+        belotes = self.get_belotes(hand)
+        announcements = []
+        #TO DO
 
-	def are_consecutive(self, first, second):
-		for i in range(len(list_of_faces)):
-			if first.face == list_of_faces[i]:
-				first_idx = i
-			if second.face == list_of_faces[i]:
-				second_idx = i
+    def are_consecutive(self, first, second):
+        for i in range(len(list_of_faces)):
+            if first.face == list_of_faces[i]:
+                first_idx = i
+            if second.face == list_of_faces[i]:
+                second_idx = i
 
-		return second_idx - first_idx == 1
+        return second_idx - first_idx == 1
--- carre.py
+++ carre.py
@@ -1,14 +1,23 @@
 class Carre:
-	def __init__(self, cards):
-		self.cards = cards
+    def __init__(self, cards):
+        self.cards = cards
 
-	def __int__(self):
-		if self.cards[0].face == 'J':
-			return 200
-		if self.cards[0].face == '9':
-			return 150
-		else:
-			return 100
+    def __int__(self):
+        if self.cards[0].face == 'J':
+            return 200
+        if self.cards[0].face == '9':
+            return 150
+        else:
+            return 100
 
-	def __repr__(self):
-		return 'carre'
+    def __repr__(self):
+        for i in range(len(self.cards)):
+            print(self.cards[i])
+        return 'carre'
+
+
+    def __eq__(self, other):
+        return self.cards == other.cards
+
+    def __str__(self):
+        return 'carre'
