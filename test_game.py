065fd6d461aced6fde57a28ceb13a3f7b773df26
Zlatina Cholakova <zlatinatc99@gmail.com>
2020-03-30 12:47:45 +0300

    test_game

---
+++ test_game.py
@@ -0,0 +1,58 @@
+import unittest
+from game import Game
+from player import Player
+from team import Team
+from belote import Belote
+from deck import Deck
+from dealer import Dealer
+from announcement import Announcement
+import random
+from consecutive import Consecutive
+
+class TestGame(unittest.TestCase):
+    def test_create_game_with_no_team(self):
+        exc = None
+        t = Team("Cats",Player("Misho"),Player("Pesho"))
+        try :
+            Game(Player("Ivan"),t)
+        except TypeError as error:
+            exc = error
+        self.assertIsNotNone(exc)
+
+    def test_create_game_with_teams_with_common_player(self):
+        exc = None
+        t1 = Team("Cats",Player("Misho"),Player("Pesho"))
+        t2 = Team("Dogs",Player("Misho"),Player("Ivanka"))
+        try:
+            g = Game(t1,t2)
+        except TypeError as error:
+            exc = error
+        self.assertIsNotNone(exc)
+
+    def test_spin_players(self):
+        exc = None
+        t1 = Team("Cats",Player("Maria"),Player("Pesho"))
+        t2 = Team("Dogs",Player("Misho"),Player("Ivanka"))
+        try:
+            g = Game(t1,t2)
+        except TypeError as error:
+            exc = error
+        first_order = g.print_table_players()
+        g.spin_players()
+        second_order = g.print_table_players()
+        self.assertNotEqual(first_order,second_order)
+
+    def test_game(self):
+        exc = None
+        t1 = Team("Cats",Player("Maria"),Player("Pesho"))
+        t2 = Team("Dogs",Player("Misho"),Player("Ivanka"))
+        g = Game(t1,t2)
+        g.round()
+
+
+    
+
+
+
+if __name__ == '__main__':
+    unittest.main()
